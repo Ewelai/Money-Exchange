a802a8d31c5136dcd901dac23acbcdf21beff588
Ewelai <palevo.ewelai@gmail.com>
2018-09-25 00:30:04 +0300

    Ewelai

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+
+    let result = {};
+    let H, Q, D, N, P;
+
+    if(currency > 10000){
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    } else if(currency <= 0) {
+        return {};
+    }
+
+    H = Math.floor(currency / 50);  if(H >= 1) result.H = H;
+    Q = Math.floor((currency - H * 50) / 25);  if(Q >= 1) result.Q = Q;
+    D = Math.floor((currency - Q * 25 - H * 50) / 10);  if(D >= 1) result.D = D;
+    N = Math.floor((currency - D * 10 - Q * 25 - H * 50) / 5);  if(N >= 1) result.N = N;
+    P = Math.floor(currency % 5);  if(P >= 1) result.P = P;
+
+    return result;
 }
